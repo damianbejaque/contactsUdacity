80d3ddaf3dcafc11f56931bb900ef9b660b30b64
damianbejaque <damianbejaque@gmail>
2020-10-31 18:55:06 -0500

    before react router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import PropTypes from 'prop-types'
-
-
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContacts from './CreateContacts'
 class App extends Component {
   state = {
-    contacts : [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts : [],
+    screen: 'create'
+  }
+  componentDidMount(){
+    ContactsAPI.getAll()
+    .then( contacts => {
+      this.setState( () => ({
+        contacts
+      }))
+    })
   }
 
   removeContact = (contact) => {
@@ -33,12 +23,18 @@ class App extends Component {
         return c.id !== contact.id
       })
     }))
+    ContactsAPI.remove(contact)
   }
   render() {
     return (
       <div>
-       <ListContacts contacts={this.state.contacts} 
-       onDeleteContact={this.removeContact}/>
+       {this.state.screen === 'list' && 
+       (<ListContacts contacts={this.state.contacts} 
+       onDeleteContact={this.removeContact}/>)
+       }
+       {this.state.screen=== 'create' &&
+       (<CreateContacts />)
+       }
       </div>
     );
   }
